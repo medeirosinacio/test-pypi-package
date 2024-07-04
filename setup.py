546141d6b625f7b0fc6dddd251287eff29c33e9d
Douglas Medeiros <eu@douglasmedeiros.dev>
2024-07-04 20:39:24 -0300

    ci: test auto release

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     readme = fh.read()
 
-setup(name='dumppackage',
-    version='0.0.3',
+setup(
+    ## Not edited. Release is generated automatically by GitHub Actions workflow from Release Publisher
+    version='0.0.4',
+    ######################################################################
+    name='dumppackage',
     url='https://github.com/medeirosinacio/test-pypi-package',
     license='MIT License',
     author='Douglas Medeiros',
@@ -14,4 +17,5 @@ setup(name='dumppackage',
     keywords='Pacote',
     description=u'Example of a package to be published in PyPI',
     packages=['dumppackage'],
-    install_requires=['numpy'],)
+    install_requires=['numpy'],
+ )
